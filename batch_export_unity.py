3bc342a616a91007ad4338fa256c2ca307bb3578
Arda Hamamcıoğlu <ardahamamcioglu@rubygamestudio.com>
2019-07-16 14:07:22 +0300

    Update for blender 2.8 compatibility, 2.79 is no longer supported.
    Also i would not recommend downloading just yet. Added an export list item to the file menu but not the export menu.

--- batch_export_unity.py
+++ batch_export_unity.py
@@ -1,58 +1,55 @@
 bl_info = {
-    "name": "Unity Batch Exporter",
-    "description": "Exports selected objects as individual fbx files for Unity game engine.",
-    "author": "Arda Hamamcıoğlu",
-    "version": (1, 0),
-    "blender": (2, 65, 0),
-    "support": "COMMUNITY",
-    "category": "Import-Export"
+	"name": "Unity Batch Exporter",
+	"description": "Exports selected objects as individual fbx files for Unity game engine.",
+	"author": "Arda Hamamcıoğlu",
+	"version": (1, 1),
+	"support": "COMMUNITY",
+	"category": "Import-Export"
 }
 
 import bpy
+from bpy import context
 import os
 
 class UnityBatchExport(bpy.types.Operator):
-    bl_idname = "object.unity_batch_export"
-    bl_label = "Export Selected Objects For Unity"
-    
-    def execute(self,context):
-        # export to blend file location
-        basedir = os.path.dirname(bpy.data.filepath)
-        exportdir = basedir+"/UnityExports"
-        if not os.path.isdir(exportdir):
-            os.mkdir(exportdir)
+	bl_idname = "object.unity_batch_export"
+	bl_label = "Unity Batch Exporter"
+	
+	def execute(self,context):
+		# export to blend file location
+		basedir = os.path.dirname(bpy.data.filepath)
+		exportdir = basedir+"/UnityExports"
+		if not os.path.isdir(exportdir):
+			os.mkdir(exportdir)
 
-        if not exportdir:
-            raise Exception("Blend file is not saved")
+		if not exportdir:
+			raise Exception("Blend file is not saved")
 
-        scene = bpy.context.scene
+		scene = context.scene
 
-        obj_active = scene.objects.active
-        selection = bpy.context.selected_objects
+		selection = context.selected_objects
 
-        bpy.ops.object.select_all(action='DESELECT')
+		for obj in selection:
 
-        for obj in selection:
+			name = bpy.path.clean_name(obj.name)
+			fn = os.path.join(exportdir, name)
+			bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+			bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, bake_space_transform=True, axis_forward="-Z",axis_up="Y",apply_scale_options="FBX_SCALE_ALL")
 
-            obj.select = True
+			print("written:", fn)
 
-            # some exporters only use the active object
-            scene.objects.active = obj
+		return{'FINISHED'}
 
-            name = bpy.path.clean_name(obj.name)
-            fn = os.path.join(exportdir, name)
-            bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
-            bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, bake_space_transform=True, axis_forward="-Z",axis_up="Y",apply_scale_options="FBX_SCALE_ALL")
-            obj.select = False
+def menu_func(self,context):
+   self.layout.operator(UnityBatchExport.bl_idname)
 
-            print("written:", fn)
-
-        scene.objects.active = obj_active
-
-        for obj in selection:
-            obj.select = True
-        return{'FINISHED'}
 def register():
-	bpy.utils.register_class(UnityBatchExport)
+   bpy.utils.register_class(UnityBatchExport)
+   bpy.types.TOPBAR_MT_fiappend(menu_func)
+
 def unregister():
 	bpy.utils.unregister_class(UnityBatchExport)
+# This allows you to run the script directly from Blender's Text editor
+# to test the add-on without having to install it.
+if __name__ == "__main__":
+	register()
