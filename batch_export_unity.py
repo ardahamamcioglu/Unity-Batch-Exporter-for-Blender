4b115264dd4a8cb952a43dc609167b765cf25e75
Arda Hamamcıoğlu <ardahamamcioglu@gmail.com>
2019-11-08 18:18:43 +0300

    Adding sync options for export
    Added a bool, created a pointer to handle strings and bools.

--- batch_export_unity.py
+++ batch_export_unity.py
@@ -1,25 +1,23 @@
 bl_info = {
-	"name": "Unity Batch Exporter",
-	"description": "Exports objects directly into the unity project respecting collection hierarchy and ignore flags.",
-	"author": "Arda Hamamcıoğlu",
-	"version": (2, 0, 5),
-	"blender" : (2, 80, 0),
-	"support": "COMMUNITY",
-	"category": "Import-Export"
+"name": "Unity Batch Exporter",
+"description": "Exports objects directly into the unity project respecting collection hierarchy and ignore flags.",
+"author": "Arda Hamamcıoğlu",
+"version": (2, 0, 5),
+"blender" : (2, 80, 0),
+"support": "COMMUNITY",
+"category": "Import-Export"
 }
 
 import bpy
 from bpy import context
-from bpy.props import (StringProperty,
-					   PointerProperty,
-					   )
-from bpy.types import (Panel,
-					   Operator,
-					   AddonPreferences,
-					   PropertyGroup,
-					   )
+from bpy.props import (StringProperty,PointerProperty,BoolProperty,StringProperty)
+from bpy.types import (Panel,Operator,AddonPreferences,PropertyGroup)
 import os
 import shutil
+
+class MySettings(PropertyGroup):
+	my_bool = BoolProperty(name="Enable or Disable",description="A bool property",default = False)
+	project_path = StringProperty(name="Project Path", default = "", description = "Navigate to the Unity project file.", subtype = 'DIR_PATH')
 
 class UnityExporterPanel(Panel):
 	bl_idname = "OBJECT_PT_unity_export_panel"
@@ -31,80 +29,85 @@ class UnityExporterPanel(Panel):
 
 	def draw(self, context):
 		layout = self.layout
-		scn = context.scene
+		scene = context.scene
+		mytool = scene.my_tool
 
 		row = layout.box()
 		row.label(text="1)Select Unity Project Folder:")
-		
-		row.prop(scn, "project_path",icon ='FILE_IMAGE')
-		
+
+		row.prop(scene, "project_path",icon ='FILE_IMAGE')
+
 		row = layout.box()
 		row.label(text="2)Hit The Button To Export:")
-		
+
 		row.operator("object.unity_batch_export")
-		
+
+		row = layout.box()
+		row.prop(mytool,"my_bool", text="Sync Deleted Files")
+
 		row = layout.row()
 		row.label(text="Add * in Collection name to ignore.")
 
 class UnityBatchExport(Operator):
-	bl_idname = "object.unity_batch_export"
-	bl_label = "Unity Batch Exporter"
-	projectPath = ""
+				bl_idname = "object.unity_batch_export"
+				bl_label = "Unity Batch Exporter"
 
-	def execute(self,context):
-		scene = context.scene
-		viewLayer = context.view_layer
-		collections = scene.collection.children
+				def execute(self,context):
+					scene = context.scene
+					viewLayer = context.view_layer
+					collections = scene.collection.children
 
-		projectdir = bpy.path.abspath(scene.project_path)
+					if scene.project_path.strip():
+						projectDir = bpy.path.abspath(scene.project_path)
+					else:
+						raise Exception("No project path selected.")
 
-		if not os.path.isdir(projectdir):
-			raise Exception("No project path selected.")
-		
-		exportdir = projectdir +"Assets/Models"
-		
-		if not os.path.isdir(exportdir):
-			os.makedirs(exportdir)
-			
-		if not exportdir:
-			raise Exception("Blend file has not been not saved yet.")
-		
-		bpy.ops.object.select_all(action='DESELECT')
-		
-		for collection in collections:
-			collectionPath = exportdir +"/" + collection.name
-			
-			if len(collection.objects)==0 or "*" in collection.name:
-				collectionPath = collectionPath.replace("*","")
-				print(collectionPath)
-				
-				if os.path.isdir(collectionPath):
-					shutil.rmtree(collectionPath)
-					
-			else:
-				if not os.path.isdir(collectionPath):
-					os.mkdir(collectionPath)
-					
-				for obj in collection.objects:
-					obj.select_set(True)
-					name = obj.name
-					fn = os.path.join(collectionPath,name)
-					bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
-					bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, bake_space_transform=True,axis_forward="-Z",axis_up="Y",apply_scale_options="FBX_SCALE_ALL",check_existing=True,filter_glob="*.fbx",bake_anim=True,armature_nodetype='NULL',bake_anim_use_all_actions=True,embed_textures=False,object_types={'MESH'})
-					obj.select_set(False)
-					print("written:", fn)
-			
-		return{'FINISHED'}
+					if not os.path.isdir(projectDir):
+						raise Exception("No project path is invalid.")
+
+					exportdir = projectDir +"Assets/Models"
+
+					if not os.path.isdir(exportdir):
+						os.makedirs(exportdir)
+
+					bpy.ops.object.select_all(action='DESELECT')
+
+					for collection in collections:
+						collectionPath = exportdir +"/" + collection.name
+
+						if len(collection.objects)==0 or "*" in collection.name:
+							collectionPath = collectionPath.replace("*","")
+							print(collectionPath)
+
+							if os.path.isdir(collectionPath):
+								shutil.rmtree(collectionPath)
+						else:
+							if not os.path.isdir(collectionPath):
+								os.mkdir(collectionPath)
+
+							for obj in collection.objects:
+								obj.select_set(True)
+								name = obj.name
+								fn = os.path.join(collectionPath,name)
+								bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+								bpy.context.object.hide_viewport = False
+								bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, bake_space_transform=True,axis_forward="-Z",axis_up="Y",apply_scale_options="FBX_SCALE_ALL",check_existing=True,filter_glob="*.fbx",bake_anim=True,armature_nodetype='NULL',bake_anim_use_all_actions=True,embed_textures=False,object_types={'MESH'})
+								obj.select_set(False)
+								print("written:", fn)
+
+					return{'FINISHED'}
 
 def register():
 	bpy.utils.register_class(UnityExporterPanel)
 	bpy.utils.register_class(UnityBatchExport)
-	bpy.types.Scene.project_path = bpy.props.StringProperty(name="Project Path", default = "", description = "Navigate to the Unity project file.", subtype = 'DIR_PATH')
+	bpy.utils.register_class(MySettings)
+	bpy.types.Scene.my_tool = PointerProperty(type=MySettings)
 
 def unregister():
 	bpy.utils.unregister_class(UnityExporterPanel)
 	bpy.utils.unregister_class(UnityBatchExport)
-	del bpy.types.Scene.project_path
+	bpy.utils.register_module(MySettings)
+	del bpy.types.Scene.my_tool
 
 if __name__ == "__main__":
 	register()
